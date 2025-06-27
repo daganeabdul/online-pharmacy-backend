0030ce2a6f500303936f7402e2c5c92c5df7ad54
daganeabdul <daganeabdulrahman@gmail.com>
2025-06-27 20:14:36 +0300

    Fix server.js and ensure correct files for Render

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-// server.js
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080;
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-server.listen(port, () => {
-  console.log(` JSON Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`JSON Server running on port ${PORT}`);
 });
+
